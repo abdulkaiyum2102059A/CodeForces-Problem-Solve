9707f9006032a78ef8533c7bc8d3ab8a24f8f600
Md Abdul Kaiyum <159060312+abdulkaiyum2102059A@users.noreply.github.com>
2024-03-21 01:17:25 +0600

    Add files via upload

---
+++ Buttons.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin >>a>>b>>c;
+
+
+        if(a!=b)
+        {
+            if(a>b)
+            {
+                cout<<"First"<<endl;
+            }
+            else
+            {
+                cout<<"Second"<<endl;
+            }
+
+        }
+
+        else
+        {
+            if(c%2==0)
+            {
+                cout<<"Second"<<endl;
+            }
+            else
+            {
+                cout<<"First"<<endl;
+            }
+
+
+
+        }
+    }
