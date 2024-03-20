9391784708a0bc571905cbee1cd1ed56946dacd2
Md Abdul Kaiyum <159060312+abdulkaiyum2102059A@users.noreply.github.com>
2024-03-21 01:20:27 +0600

    Add files via upload

---
+++ How much does daytona cost.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+       int a[n];
+       int count =0;
+       for(int i=0;i<n;i++){
+        cin>>a[i];
+        if(a[i]==k){
+            count++;
+        }
+       }
+       if(count>0){
+        cout<<"YES"<<endl;
+       }else{
+       cout<<"NO"<<endl;
+       }
+    }
+}
+
