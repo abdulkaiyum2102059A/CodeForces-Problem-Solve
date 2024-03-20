81f0301fc2a79f6a726a51a5a605df345c7fa2a7
Md Abdul Kaiyum <159060312+abdulkaiyum2102059A@users.noreply.github.com>
2024-03-21 01:38:21 +0600

    Add files via upload

---
+++ odd one set.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--){
+        int a[3];
+        cin>>a[0]>>a[1]>>a[2];
+        sort(a,a+3);
+
+        if(a[0]==a[1]){
+        cout<<a[2]<<endl;
+    }else{
+      cout<<a[0]<<endl;
+    }
+    }
+
+}
+
+
