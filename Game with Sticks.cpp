564120ff8c695af8df909abe062fc499d204cdd2
Md Abdul Kaiyum <159060312+abdulkaiyum2102059A@users.noreply.github.com>
2024-03-21 01:24:33 +0600

    Add files via upload

---
+++ Game with Sticks.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,l;
+    cin>>n>>l;
+
+    if((n*l)%2==0&&(n*l)!=2){
+        cout<<"Malvika";
+    }else{
+    cout<<"Akshat";
+    }
+}
