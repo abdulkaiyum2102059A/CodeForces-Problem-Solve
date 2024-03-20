f096189b3a4349c91f88d1b72ea87a51c7151570
Md Abdul Kaiyum <159060312+abdulkaiyum2102059A@users.noreply.github.com>
2024-03-21 01:46:51 +0600

    Add files via upload

---
+++ codeforces checking.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    char c;
+    while(n--){
+            cin>>c;
+            if(c=='c'||c=='o'||c=='d'||c=='e'||c=='f'||c=='r'||c=='s'){
+                cout<<"YES"<<endl;
+            }else{
+            cout<<"NO";
+            }
+
+    }
+}
